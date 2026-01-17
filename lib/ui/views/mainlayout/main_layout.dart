4409cca727d9d4d6bc0b33ddb4ed0c68482a743d
HoogaBoga <matty.lim718@gmail.com>
2026-01-17 21:23:11 +0800

    Changed main layout to add stats page view

--- lib/ui/views/mainlayout/main_layout.dart
+++ lib/ui/views/mainlayout/main_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_gaia/ui/views/stats/stats_page_view.dart';
 import 'package:project_gaia/ui/widgets/bottom_navbar.dart';
-import 'package:project_gaia/ui/views/home/home_view.dart'; 
+import 'package:project_gaia/ui/views/home/home_view.dart';
 import 'package:project_gaia/ui/views/settings/settings_view.dart';
 
 class MainLayout extends StatefulWidget {
@@ -17,15 +17,12 @@ class _MainLayoutState extends State<MainLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0A2342), 
+      backgroundColor: const Color(0xFF0A2342),
       body: IndexedStack(
         index: _currentIndex,
         children: const [
-          HomeView(), 
-          
-          Center(
-            child: Text("Stats Page", style: TextStyle(color: Colors.white)),
-          ),
+          HomeView(),
+          StatsPageView(),
           Center(
             child: Text("Chat Page", style: TextStyle(color: Colors.white)),
           ),
