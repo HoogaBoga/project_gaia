f8af17e9c0013df6824d0ae2ed6c3474d72f77b7
toot-a-loot <mjartgithub@gmail.com>
2026-01-18 21:51:40 +0800

    chore : connect Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,17 @@ import 'package:project_gaia/app/app.locator.dart';
 import 'package:project_gaia/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
